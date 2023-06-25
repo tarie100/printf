3627c698d2557330470f7c9b988b72ea2f8e1cba
tarie100 <tarie.school@gmail.com>
2023-06-25 05:43:42 -0700

    task0

---
+++ file_p1.c
@@ -0,0 +1,44 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdarg.h>
+/**
+ * _printf -  produces output according to a format.
+ * @format: character string
+ * Return: number of chars printed
+ */
+int _printf(const char *format, ...)
+{
+	int a;
+	va_list args;
+
+	va_start(args, format);
+	a = 0;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case ('c'):
+					a += printf("%c", va_arg(args, int));
+					break;
+				case ('s'):
+					    a += printf("%s", va_arg(args, char *));
+					break;
+				case ('%'):
+					    a += printf("%%");
+					break;
+				default:
+					    a += printf("?");
+			}
+		}
+		else
+		{
+			a += printf("%c", *format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (a);
+}
