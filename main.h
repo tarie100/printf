11570912f7001dbaeef553ebfa1d23a310f9ce21
tarie100 <tarie.school@gmail.com>
2023-06-26 04:05:21 -0700

    task10

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _printf(const char *format, ...);
+int _handle(const char *format, ...);
+void format_dbl(double a, int precision, char *str);;
+int _putchar(void);
+/** Custom Binary**/
+void print_binary(unsigned int n);
+void custom_printf(const char *format, ...);
+#endif
