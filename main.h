c7ec50656fe43cc2685751f54e126e19dc1a7a17
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-06-26 02:19:15 -0700

    task2

--- main.h
+++ main.h
@@ -3,4 +3,8 @@
 #include<unistd.h>
 int _printf(const char *format, ...);
 int _handle(const char *format, ...);
+
+/** Custom Binary **/
+void print_binary(unsigned int n);
+void custom_printf(const char *format, ...);
 #endif
